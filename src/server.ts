d2d0c9c05e82dc88f0044079ea1750e09d6b9da6
Justin Hill <justin@jmhill.dev>
2025-01-27 23:50:53 -0500

    change placeholder message

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import { Hono } from "hono";
 const app = new Hono();
 
-app.get("/", (ctx) => ctx.text("Hello world, this is Hono!!"));
+app.get("/", (ctx) => ctx.text("Under construction, check back later!"));
 
 export default app;
